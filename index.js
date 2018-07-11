babfc275f332d1219072b0a2e5db0f2b4de635d5
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-07-11 05:36:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-function happyHolidays() {return("Happy holidays!") }
+function happyHolidays() { return("Happy holidays!") }
+function happyHolidaysTo(name) { return(`Happy holidays, ${name}!`) }
+function happyHolidayTo(hil)
