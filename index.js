3379c102143d25df7f1abf8a77d72708af0edf85
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-07-11 05:40:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 function happyHolidays() { return("Happy holidays!") }
 function happyHolidaysTo(name) { return(`Happy holidays, ${name}!`) }
-function happyHolidayTo(hil)
+function happyHolidayTo(holiday, name) { return(`Happy ${holiday}, ${name}!`) }
+function holidayCountdown(holiday, )
